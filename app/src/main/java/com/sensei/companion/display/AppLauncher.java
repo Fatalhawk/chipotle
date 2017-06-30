99f95444af6dafa73cfe1e9199978fdfccd01a59
Salar Hosseini <salar77h@gmail.com>
2017-06-29 23:32:29 -0400

    fixing conflicts

--- app/src/main/java/com/sensei/companion/display/AppLauncher.java
+++ app/src/main/java/com/sensei/companion/display/AppLauncher.java
@@ -1,5 +1,6 @@
 package com.sensei.companion.display;
 
+import android.content.Intent;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,142 +29,15 @@ public class AppLauncher extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_launcher);
-        View v = findViewById(android.R.id.content);
-        setUpGestures(v);
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+        //setContentView(R.layout.activity_app_launcher);
+
+        //print network stuff
+
+        startProgram ();
     }
 
-    public boolean onTouchEvent(MotionEvent event){
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    public void setUpGestures(View v){
-        NewGestureManager mySfg = new NewGestureManager();
-        mySfg.setDebug(false);
-        mySfg.setConsumeTouchEvents(true);
-        mySfg.setOnFingerGestureListener(new NewGestureManager.OnFingerGestureListener() {
-            @Override
-            public boolean onSwipeUp(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 1){
-                    Log.i(DEBUG_TAG, "swipe up with " + fingers + " fingers");
-                    //action
-                }
-                if(fingers == 2){
-                    Log.i(DEBUG_TAG, "swipe up with " + fingers + " fingers");
-                    //action
-                }
-                else if(fingers == 3){
-                    Log.i(DEBUG_TAG, "swipe up with " + fingers + " fingers");
-                    //action
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "swipe up with " + fingers + " fingers");
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onSwipeDown(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 1){
-                    Log.i(DEBUG_TAG, "swipe down with " + fingers + " fingers");
-                    //action
-                }
-                if(fingers == 2){
-                    Log.i(DEBUG_TAG, "swipe down with " + fingers + " fingers");
-                    //action
-                }
-                else if(fingers == 3){
-                    Log.i(DEBUG_TAG, "swipe down with " + fingers + " fingers");
-                    //action
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "swipe down with " + fingers + " fingers");
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onSwipeLeft(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 1){
-                   Log.i(DEBUG_TAG, "swipe left with " + fingers + " fingers");
-                    //action
-                }
-                if(fingers == 2){
-                    //action
-                    Log.i(DEBUG_TAG, "swipe left with " + fingers + " fingers");
-                }
-                else if(fingers == 3){
-                    //action
-                    Log.i(DEBUG_TAG, "swipe left with " + fingers + " fingers");
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "swipe left with " + fingers + " fingers");
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onSwipeRight(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 1){
-                   Log.i(DEBUG_TAG, "swipe right with " + fingers + " fingers");
-                    //action
-                }
-                if(fingers == 2){
-                    //action
-                    Log.i(DEBUG_TAG, "swipe right with " + fingers + " fingers");
-                }
-                else if(fingers == 3){
-                    //action
-                    Log.i(DEBUG_TAG, "swipe right with " + fingers + " fingers");
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "swipe right with " + fingers + " fingers");
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onPinch(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 2){
-                    //action
-                    Log.i(DEBUG_TAG, "pinch in with " + fingers + " fingers");
-                }
-                else if(fingers == 3){
-                    //action
-                    Log.i(DEBUG_TAG, "pinch in with " + fingers + " fingers");
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "pinch in with " + fingers + " fingers");
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onUnpinch(int fingers, double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration) {
-                if(fingers == 2){
-                    //action
-                    Log.i(DEBUG_TAG, "pinch out with " + fingers + " fingers");
-                }
-                else if(fingers == 3){
-                    //action
-                    Log.i(DEBUG_TAG, "pinch out with " + fingers + " fingers");
-                }
-                else if(fingers == 4){
-                    Log.i(DEBUG_TAG, "pinch out with " + fingers + " fingers");
-                }
-                return false;
-            }
-            public boolean onTwoFingerTap(double[] initialX, double[] initialY, double[] finalX, double[] finalY, long gestureDuration){
-                Log.i(DEBUG_TAG, "two finger tap");
-                return false;
-            }
-        });
-        v.setOnTouchListener(mySfg);
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void startProgram () {
+        Intent i = new Intent(this, PcManager.class);
+        startActivity(i);
     }
 }
