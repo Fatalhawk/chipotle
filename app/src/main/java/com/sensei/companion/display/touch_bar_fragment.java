7476fcbe312d15edbca80fc3208b2d1225305230
rojigan98 <rojigan98@gmail.com>
2017-07-10 08:51:46 -0400

    Added ability to change images upon button click. Not done yet.

--- app/src/main/java/com/sensei/companion/display/touch_bar_fragment.java
+++ app/src/main/java/com/sensei/companion/display/touch_bar_fragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageButton;
+import android.graphics.drawable.Drawable;
+import android.
 
 public class touch_bar_fragment extends Fragment{
 
@@ -15,5 +17,22 @@ public class touch_bar_fragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.touch_bar_fragment, container, false);
+        final ImageButton imageButton7 =  (ImageButton) view.findViewById(R.id.imageButton7);
+        imageButton7.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //once I set the play.png and pause.png properly I'm good for the touchbar fragment
+                if(imageButton7.getTag() != null && imageButton7.getTag().toString().equals("pause.png")){
+                    imageButton7.setImageResource(R.drawable.play.png);
+                    imageButton7.setTag("play.png");
+                } else {
+                    imageButton7.setImageResource(R.drawable.pause.png);
+                    imageButton7.setTag("pause.png");
+                }
+            }
+
+
+
+        });
+
     }
 }
