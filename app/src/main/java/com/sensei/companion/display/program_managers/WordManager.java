70590205c5b0dbc7aa300b6473028e1b3c02b2ee
Anup Deb <ovik_123@hotmail.com>
2017-09-01 22:48:45 -0400

    made symbols segmented button

--- app/src/main/java/com/sensei/companion/display/program_managers/WordManager.java
+++ app/src/main/java/com/sensei/companion/display/program_managers/WordManager.java
@@ -5,16 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import co.ceryle.radiorealbutton.RadioRealButton;
+import co.ceryle.radiorealbutton.RadioRealButtonGroup;
 
 import com.sensei.companion.R;
 
 public class WordManager extends TouchBarFragment {
 
+    private RadioRealButtonGroup group2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_word_manager, container, false);
+        group2.setOnClickedButtonListener(new RadioRealButtonGroup.OnClickedButtonListener() {
+            @Override
+            public void onClickedButton(RadioRealButton button, int position) {
+                // do stuff
+            }
+        });
+
 
     }
 
