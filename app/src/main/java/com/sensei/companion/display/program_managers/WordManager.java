ebab362869b0a40e118bab6c7ca949695020c72a
Anup Deb <ovik_123@hotmail.com>
2017-09-01 23:14:01 -0400

    added color selector

--- app/src/main/java/com/sensei/companion/display/program_managers/WordManager.java
+++ app/src/main/java/com/sensei/companion/display/program_managers/WordManager.java
@@ -1,7 +1,10 @@
 package com.sensei.companion.display.program_managers;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,10 @@ import android.widget.Toast;
 
 import co.ceryle.radiorealbutton.RadioRealButton;
 import co.ceryle.radiorealbutton.RadioRealButtonGroup;
+
+
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 
 import com.sensei.companion.R;
 
@@ -26,6 +33,29 @@ public class WordManager extends TouchBarFragment {
                 // do stuff
             }
         });
+
+        // color stuff first argument is view
+        final ColorPicker cp = new ColorPicker(inflater.inflate(R.layout.fragment_word_manager, container, false), 255, 255, 255);
+        cp.show();
+
+        cp.setCallback(new ColorPickerCallback() {
+            @Override
+            public void onColorChosen(@ColorInt int color) {
+                // Do whatever you want
+                // Examples
+                Log.d("Alpha", Integer.toString(Color.alpha(color)));
+                Log.d("Red", Integer.toString(Color.red(color)));
+                Log.d("Green", Integer.toString(Color.green(color)));
+                Log.d("Blue", Integer.toString(Color.blue(color)));
+
+                Log.d("Pure Hex", Integer.toHexString(color));
+                Log.d("#Hex no alpha", String.format("#%06X", (0xFFFFFF & color)));
+                Log.d("#Hex with alpha", String.format("#%08X", (0xFFFFFFFF & color)));
+                cp.dismiss();
+            }
+        });
+
+
 
 
     }
